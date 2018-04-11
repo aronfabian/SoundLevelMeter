f45be9d4ec6f50fc9d33f30485e14c7a827f7c44
aronfabian <aron.sajt@gmail.com>
2018-04-11 19:19:50 +0200

    new: DB, DeviceName, UploadJobService

---
+++ app/src/main/java/wavrecorder/com/fabian/aron/wavrecorder/CalibrationType.java
@@ -0,0 +1,10 @@
+package wavrecorder.com.fabian.aron.wavrecorder;
+
+/**
+ * Created by Aron Fabian on 2018. 04. 06..
+ */
+public enum CalibrationType {
+    NOT_CALIBRATED,
+    MODELLY_CALIBRATED,
+    UNIQUELY_CALIBRATED
+}
