daf286d250cb13115e6600148eb0ecaa42e30c73
aronfabian <aron.sajt@gmail.com>
2018-09-15 16:12:16 +0200

    kalibrációs activity state 1

---
+++ app/src/main/java/wavrecorder/com/fabian/aron/wavrecorder/CalibrationActivity.java
@@ -0,0 +1,15 @@
+package wavrecorder.com.fabian.aron.wavrecorder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Aron Fabian on 2018. 09. 15..
+ */
+public class CalibrationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calibration);
+    }
+}
